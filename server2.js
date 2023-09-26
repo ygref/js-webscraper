af9e6dc66a65cfa2e8376ff508863c77faa36586
ygref <jason.wesley.hc@gmail.com>
2023-09-26 14:57:41 -0400

    got it

---
+++ server2.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+async function scrapeWebsite(url) {
+  try {
+    // Fetch the HTML content of the webpage
+    const response = await axios.get(url);
+    const html = response.data;
+
+    // Load HTML content into Cheerio
+    const $ = cheerio.load(html);
+
+    const extractedText = [];
+    $("h2").each((index, element) => {
+      // Adjust the selector according to the HTML structure
+      const text = $(element).text().trim();
+      extractedText.push(text);
+    });
+
+    // Process and display the extracted text
+    console.log("Extracted Text:", extractedText);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+// URL of the webpage you want to scrape
+const targetURL = "https://scrapeme.live/shop/";
+
+// Call the scraping function with the target URL
+scrapeWebsite(targetURL);
