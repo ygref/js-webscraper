f08de6d8137df467eeb55feaa6d2ecc3194a297a
ygref <jason.wesley.hc@gmail.com>
2023-10-28 16:13:27 -0400

    lots of changes with readline

--- server3.js
+++ server3.js
@@ -1,9 +1,17 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const readline = require("readline");
 
-async function scrapeWebsite(url) {
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function scrapeWebsite(cardSet, cardName) {
+  const targetURL = `https://www.mtggoldfish.com/price/${encodeURIComponent(cardSet)}/${encodeURIComponent(cardName)}`;
+
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(targetURL);
     const html = response.data;
 
     const $ = cheerio.load(html);
@@ -26,13 +34,28 @@ async function scrapeWebsite(url) {
     });
 
     console.log("Extracted Text:", extractedText);
-    console.log("Extraced Price:", extractedPrice);
-    console.log("Purchase Price:", purchasePrice);
+    // console.log("Extracted Price:", extractedPrice);
+    console.log("Card Kingdom:", purchasePrice[0]);
+    console.log("eBay - But It Now:", purchasePrice[1]);
+    console.log("TCGplayer Mid:", purchasePrice[2]);
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
-const targetURL = "https://www.mtggoldfish.com/price/Gatecrash/Prime+Speaker+Zegana#online";
+function searchForCard() {
+  rl.question("Enter the card name: (Case sensitive)", (cardName) => {
+    rl.question("Enter the set: (Case sensitive)", (cardSet) => {
+      scrapeWebsite(cardSet, cardName);
+      rl.question("Do you want to look for another card? (Yes / No): ", (answer) => {
+        if (answer.toLowerCase() === "yes") {
+          searchForCard();
+        } else {
+          rl.close();
+        }
+      });
+    });
+  });
+}
 
-scrapeWebsite(targetURL);
+searchForCard();
